41bca75641319fe69f9a2d1126ff3e56e32fdd87
Ajay <aerra@financialoxygen.com>
2022-12-07 10:53:01 +0000

    OMS-WAR-07122022

---
+++ env.js
@@ -0,0 +1,5 @@
+window.env = {
+  "API_URL": "https://183.82.100.141:7101/omsservice/omsservice/api",
+  "WS_URL": "wss://183.82.100.141:7101/omsservice",
+  "SIGNIN_URL": "https://183.82.100.141:7101/omsservice/omsauth/signin"
+};
